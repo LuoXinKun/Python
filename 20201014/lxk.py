0c37d6a2e0c37331cbef0d2a0016f908021eafe4
lxk19920328 <1154128829@qq.com>
2020-10-14 02:08:09 +0800

    sleep

--- 20201014/lxk.py
+++ 20201014/lxk.py
@@ -7,14 +7,9 @@ def maopao(lista):
             if lista[j]>lista[i]:
                 lista[j],lista[i] = lista[i],lista[j]
                 
-<<<<<<< HEAD
+# 1014 最后一次更新
 list = [1,5,7,2,6,1,5,3]
 list = maopao(list)
 print(list)
 
-if 1:
-    print(1)
-=======
-a = maopao([1,3,2,4,5,6,2,1])
-print(a)
->>>>>>> e1cf19eb79516a4dc873312905b40ae27c25f01e
+
