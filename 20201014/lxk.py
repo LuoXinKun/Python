0518269c41c3fdad293032a32d6276e4712c43f1
lxk19920328 <1154128829@qq.com>
2020-10-14 00:22:04 +0800

    change maopao

--- 20201014/lxk.py
+++ 20201014/lxk.py
@@ -6,3 +6,7 @@ def maopao(lista):
         for j in range(i,num):
             if lista[j]>lista[i]:
                 lista[j],lista[i] = lista[i],lista[j]
+                
+list = [1,5,7,2,6,1,5,3]
+list = maopao(list)
+print(list)
